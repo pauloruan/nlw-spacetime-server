67579a769e71627122bef23be52f2458c46b46d6
Paulo Ruan <85769241+pauloruan@users.noreply.github.com>
2023-05-16 17:19:15 -0300

    feat: basic structure of the project created and dependencies installed

---
+++ src/server.ts
@@ -0,0 +1,13 @@
+import { PrismaClient } from "@prisma/client"
+import fastify from "fastify"
+
+const app = fastify()
+const prisma = new PrismaClient()
+
+app.get("/users", async () => {
+  return await prisma.user.findMany()
+})
+
+app.listen({ port: 3333 }).then(() => {
+  console.log("🚀 HTTP server running on http://localhost:3333")
+})
