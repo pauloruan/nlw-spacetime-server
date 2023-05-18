f9414829a6b9885bfa4a533be007e30786056a05
Paulo Ruan <85769241+pauloruan@users.noreply.github.com>
2023-05-18 17:33:57 -0300

    feat: configured the HTTP server, added routes for memories and enabled CORS

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,11 @@
-import { PrismaClient } from "@prisma/client"
+import cors from "@fastify/cors"
 import fastify from "fastify"
+import { memoriesRoutes } from "./routes/memories"
 
 const app = fastify()
-const prisma = new PrismaClient()
 
-app.get("/users", async () => {
-  return await prisma.user.findMany()
-})
+app.register(memoriesRoutes)
+app.register(cors, { origin: "http://localhost:3000" })
 
 app.listen({ port: 3333 }).then(() => {
   console.log("🚀 HTTP server running on http://localhost:3333")
