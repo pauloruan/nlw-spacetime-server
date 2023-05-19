327b233f611b1e6d395a8ffc20191825739fe76b
Paulo Ruan <85769241+pauloruan@users.noreply.github.com>
2023-05-19 16:51:10 -0300

    feat: jwt configured and auth route implemented

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,17 @@
 import cors from "@fastify/cors"
+import jwt from "@fastify/jwt"
+import "dotenv/config"
 import fastify from "fastify"
+import { authRoutes } from "./routes/auth"
 import { memoriesRoutes } from "./routes/memories"
 
 const app = fastify()
 
+app.register(authRoutes)
 app.register(memoriesRoutes)
 app.register(cors, { origin: "http://localhost:3000" })
+app.register(jwt, { secret: "XHtFvME1exKt7Z1Cbvzb4a+NreYSFuS7WHl2oZ9+qjI=" })
 
-app.listen({ port: 3333 }).then(() => {
+app.listen({ port: 3333, host: "0.0.0.0" }).then(() => {
   console.log("🚀 HTTP server running on http://localhost:3333")
 })
