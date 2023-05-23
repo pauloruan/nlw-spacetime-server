6a56d10c1f2892ca5082e0ae5f385120bbf32ccc
Paulo Ruan <85769241+pauloruan@users.noreply.github.com>
2023-05-23 16:25:24 -0300

    feat: Improve fastify server

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,26 @@
 import cors from "@fastify/cors"
 import jwt from "@fastify/jwt"
+import multipart from "@fastify/multipart"
 import "dotenv/config"
 import fastify from "fastify"
+import { resolve } from "path"
 import { authRoutes } from "./routes/auth"
 import { memoriesRoutes } from "./routes/memories"
+import { uploadRoutes } from "./routes/upload"
 
 const app = fastify()
 
-app.register(authRoutes)
-app.register(memoriesRoutes)
+app.register(multipart)
 app.register(cors, { origin: "http://localhost:3000" })
 app.register(jwt, { secret: "XHtFvME1exKt7Z1Cbvzb4a+NreYSFuS7WHl2oZ9+qjI=" })
+app.register(require("@fastify/static"), {
+  root: resolve(__dirname, "../uploads"),
+  prefix: "/uploads"
+})
+
+app.register(authRoutes)
+app.register(uploadRoutes)
+app.register(memoriesRoutes)
 
 app.listen({ port: 3333, host: "0.0.0.0" }).then(() => {
   console.log("🚀 HTTP server running on http://localhost:3333")
